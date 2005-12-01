58605b0b837b0f82595fd0ffbc9cb00024094938
Yuval Kogman <nothingmuch@woobling.org>
2005-12-01 11:40:41 +0000

    Remove Test::Plan dep for ACL

--- lib/Catalyst/Plugin/Authorization/ACL.pm
+++ lib/Catalyst/Plugin/Authorization/ACL.pm
@@ -11,7 +11,7 @@ use Catalyst::Plugin::Authorization::ACL::Engine;
 
 BEGIN { __PACKAGE__->mk_classdata("_acl_engine") }
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub execute {
     my ( $c, $class, $action ) = @_;
