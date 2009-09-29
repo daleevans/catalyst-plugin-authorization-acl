da6450be2d81a4ee8927e6cc12a21ba499c3b8ea
Rafael Kitover <rkitover@cpan.org>
2009-09-29 13:22:10 +0000

    minor changes

--- lib/Catalyst/Plugin/Authorization/ACL.pm
+++ lib/Catalyst/Plugin/Authorization/ACL.pm
@@ -14,7 +14,7 @@ use Catalyst::Plugin::Authorization::ACL::Engine;
 
 BEGIN { __PACKAGE__->mk_classdata("_acl_engine") }
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 my $FORCE_ALLOW = bless {}, __PACKAGE__ . "::Exception";
 
